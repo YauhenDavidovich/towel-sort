ba2dd16abeb45c1f9fa2283b1d7312d5a6cb63a4
Шурхай Алексей Сергеевич <a.shurkhai@ziko.loc>
2020-02-21 09:36:52 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixRow = []
+  if (!matrix) {return matrixRow}  
+  for (let i = 0; i < matrix.length; i++) {
+    if (i == 0 || i % 2 == 0 ) {
+      for (let j =0; j < matrix[i].length;j++) {
+        matrixRow.push(matrix[i][j])
+      }
+    } else {
+      for (let j = matrix[i].length -1; j >= 0; j--) { //reverse row 
+        matrixRow.push(matrix[i][j])
+      }
+    }    
+  }
+  return matrixRow;
 }
